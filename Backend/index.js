bbcea8c592b9d7633fea48da72a926ff7b4ab7f6
MelaniSandalika <79406168+MelaniSandalika@users.noreply.github.com>
2022-03-17 13:31:10 -0700

    Inventory Module

--- Backend/index.js
+++ Backend/index.js
@@ -19,10 +19,12 @@ app.use(cors({
 
 const loginRouter = require('./routes/login');
 const hrRouter = require("./routes/hr");
+const inventoryRouter = require("./routes/inventory");
 
 
 app.use("/auth", loginRouter);
 app.use("/hr", hrRouter);
+app.use("/inventory", inventoryRouter);
 
 
 const PORT = process.env.PORT || 3000;
