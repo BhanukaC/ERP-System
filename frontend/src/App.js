cbc720e6fe202f999f509909d1b40bed1d3f0644
BhanukaC <50510335+BhanukaC@users.noreply.github.com>
2022-06-04 04:09:59 +0530

    Footer, Navbar and sidebar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,18 @@
 import './App.css';
+import Navbar from './components/NavBar/navBar';
+import Footer from './components/Footer/footer';
+import Sidebar from './components/sidebar/sidebar';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Sidebar />
+      <Footer />
+    </>
   );
 }
 
