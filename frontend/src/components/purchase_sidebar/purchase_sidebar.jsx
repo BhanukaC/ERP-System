2188e97b7642014021f7f3ceeed01bb0127f6203
Nethmi-Randi <80019485+Nethmi-Randi@users.noreply.github.com>
2022-06-18 14:45:33 +0530

    purchase

--- frontend/src/components/purchase_sidebar/purchase_sidebar.jsx
+++ frontend/src/components/purchase_sidebar/purchase_sidebar.jsx
@@ -10,16 +10,16 @@ import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined
 import AddLocationIcon from '@mui/icons-material/AddLocation';
 import AddIcCallIcon from '@mui/icons-material/AddIcCall';
 import { Link } from "react-router-dom";
-import { DarkModeContext } from "../../context/darkModeContext";
+
 import {useContext } from "react";
 
 const Sidebar = () => {
-  const { dispatch } = useContext(DarkModeContext);
+ 
   return (
     <div className="sidebar">
       <div className="top">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo">ABCD</span>
+          <span className="logo">Quick ERP</span>
         </Link>
       </div>
       <hr />
@@ -134,16 +134,7 @@ const Sidebar = () => {
           </li>
         </ul>
       </div>
-      <div className="bottom">
-        <div
-          className="colorOption"
-          onClick={() => dispatch({ type: "LIGHT" })}
-        ></div>
-        <div
-          className="colorOption"
-          onClick={() => dispatch({ type: "DARK" })}
-        ></div>
-      </div>
+      
     </div>
   );
 };
