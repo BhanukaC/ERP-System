69e3cab2eca75d889982ae3eb10ce38f0c8f171a
MelaniSandalika <79406168+MelaniSandalika@users.noreply.github.com>
2022-06-25 13:47:50 -0700

    styled profile heading

--- frontend/src/pages/inventory/internalShipment/addShipment1.jsx
+++ frontend/src/pages/inventory/internalShipment/addShipment1.jsx
@@ -188,7 +188,7 @@ if(stat){
             </form>
           </div>
         </div>
-        <div className="bottom">
+        <div className="bottomContainer">
           <div className="right">
             <h1>Cart</h1>
             <table style={{ width: "80%", textAlign: "center" }}>
